a517166824fd93bc35b9feff204c60aa960e9259
Andreas Gillström <andreasgillstrom@gmail.com>
2016-04-16 10:27:22 +0200

    Bump AVA version

--- test.js
+++ test.js
@@ -2,30 +2,30 @@ import test from 'ava';
 import fn from './';
 
 test('repeated prime factors', t => {
-	t.assert(fn(5, 6));
-	t.assert(fn(8, 9));
-	t.assert(fn(15, 16));
-	t.assert(fn(77, 78));
-	t.assert(fn(125, 126));
-	t.assert(fn(714, 715));
-	t.assert(fn(948, 949));
-	t.assert(fn(1330, 1331));
-	t.assert(!fn(123, 124));
-	t.end();
+	t.true(fn(5, 6));
+	t.true(fn(8, 9));
+	t.true(fn(15, 16));
+	t.true(fn(77, 78));
+	t.true(fn(125, 126));
+	t.true(fn(714, 715));
+	t.true(fn(948, 949));
+	t.true(fn(1330, 1331));
+	t.false(fn(123, 124));
 });
 
 test('distinct prime factors', t => {
-	t.assert(fn(5, 6, {distinct: true}));
-	t.assert(fn(24, 25, {distinct: true}));
-	t.assert(fn(49, 50, {distinct: true}));
-	t.assert(fn(77, 78, {distinct: true}));
-	t.assert(fn(104, 105, {distinct: true}));
-	t.assert(fn(153, 154, {distinct: true}));
-	t.assert(fn(369, 370, {distinct: true}));
-	t.assert(fn(492, 493, {distinct: true}));
-	t.assert(fn(714, 715, {distinct: true}));
-	t.assert(fn(1682, 1683, {distinct: true}));
-	t.assert(fn(2107, 2108, {distinct: true}));
-	t.assert(!fn(123, 124, {distinct: true}));
-	t.end();
+	const opts = {distinct: true};
+
+	t.true(fn(5, 6, opts));
+	t.true(fn(24, 25, opts));
+	t.true(fn(49, 50, opts));
+	t.true(fn(77, 78, opts));
+	t.true(fn(104, 105, opts));
+	t.true(fn(153, 154, opts));
+	t.true(fn(369, 370, opts));
+	t.true(fn(492, 493, opts));
+	t.true(fn(714, 715, opts));
+	t.true(fn(1682, 1683, opts));
+	t.true(fn(2107, 2108, opts));
+	t.false(fn(123, 124, opts));
 });
